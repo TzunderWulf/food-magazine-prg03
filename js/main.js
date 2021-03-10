07206673876309fd81cb42b1b11b65ed5157bc77
TzunderWulf <svandenbos@tzunder.nl>
2021-03-10 23:25:12 +0100

    fixed issues with the javascript

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ window.addEventListener('load', init);
 let recipesSection;
 let buttonsRecipe;
 let buttonsFavorite;
+let currentClickedRecipe;
 
 function init() {
 
@@ -32,6 +33,10 @@ function showRecipe(e) {
         
     }
 
+    // get button and id of button for the right info
+    let btn = e.target;
+    let btnIndex = btn.dataset.index;
+
     let listOfRecipes = ["Put it in the oven and go!", "Cheesy Macaroni, straight out of the oven!", "Available for self-cooking or as an instant.", "Everytime in the city after midnight",
     "Specialty when on holiday in Spain", "Order or make it yourself!", "Meat (pork or chicken) served with potato, onion, tomato, tzatziki or yogurt!", "Straight out of heaven.",
     "Famous in Berlin, lovely to eat.", "Цыка блять, советы идут"];
@@ -39,11 +44,25 @@ function showRecipe(e) {
     let tags = ['cheese, oven', 'cheese, oven, mac', 'omnomnom, noodles, instant', 'kapsalon, tasty, meat', 'fish, rice', 'fish, seaweed, food', 
     'meat, fresh vegetables, pork, chicken', 'omnomnom, cheese, oven', 'bread, potato, tasty, meat', "pancake, savory, sweet",]
 
-    let detailView = document.getElementById('detail-view');
+    //let detailView = document.getElementById('detail-view');
 
-    let btn = e.target;
-    let btnIndex = btn.dataset.index;
+    // selecting all elements
+    // both divs for recipe and tags
+    let recipeDiv = document.getElementById('recipe-dish');
+    let tagsDiv = document.getElementById('dish-tags');
 
-    detailView.innerHTML = `Recipe<br>${listOfRecipes[btnIndex]}<br>Tags<br>${tags[btnIndex]}`;
+    // both headers 1 for recipe and tags
+    let recipeHeader = document.getElementById('recipe-header');
+    let recipeText = document.getElementById('recipe-text');
+
+    // both headers 2 for recipe and tags
+    let tagsHeader = document.getElementById('tags-header');
+    let tagsText = document.getElementById('tags-text');
+
+    // fill in the right info
+    recipeHeader.innerHTML = "Recipe";
+    recipeText.innerHTML = listOfRecipes[btnIndex];
+    tagsHeader.innerHTML = "Related tags";
+    tagsText.innerHTML = tags[btnIndex];
 
 }
