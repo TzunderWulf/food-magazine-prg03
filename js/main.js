d1b5cf174977eb749da0ae3c6cdb5d865695f286
TzunderWulf <svandenbos@tzunder.nl>
2021-03-19 23:56:50 +0100

    Added function to check classes of buttons

--- js/main.js
+++ js/main.js
@@ -5,84 +5,117 @@ let recipesSection;
 let buttonsRecipe;
 let buttonsFavorite;
 let currentClickedRecipe;
+let listOfRecipes = [
+    {   
+        description: 'Put it in the oven and go!',
+        tags: 'cheese, oven'
+    },
+    {
+        description: 'Cheesy Macaroni, straight out of the oven!',
+        tags: 'cheese, oven, mac'
+    },
+    {
+        description: 'Available for self-cooking or as an instant.',
+        tags: 'omnomnom, noodles, instant'
+    },
+    {
+        description: 'Everytime in the city after midnight',
+        tags: 'kapsalon, tasty, meat'
+    },
+    {
+        description: 'Specialty when on holiday in Spain',
+        tags: 'fish, rice'
+    },
+    {   
+        description: 'Order or make it yourself!',
+        tags: 'fish, seaweed, food'
+    },
+    {
+        description: 'Meat (pork or chicken) served with potato, onion, tomato, tzatziki or yogurt!',
+        tags: 'meat, fresh vegetables, pork, chicken'
+    },
+    {
+        description: 'Straight out of heaven.',
+        tags: 'omnomnom, cheese, oven'
+    },
+    {
+        description: 'Famous in Berlin, lovely to eat.',
+        tags: 'bread, potato, tasty, meat'
+    },
+    {
+        description: 'Цыка блять, советы идут',
+        tags: 'pancake, savory, sweet'
+    }
+]
+let favorites = [];
 
 function init() {
 
-    // get entire section with all recipes
+    // Get entire section with all recipes
     recipesSection = document.getElementById('recipes');
-    recipesSection.addEventListener('click', showRecipe);
+    recipesSection.addEventListener('click', clickHandler);
 
-    // get all the buttons with classname
+    // Get all the buttons with classname
     buttonsRecipe = document.getElementsByClassName('recipe-btn');
-    // toggleFavorites = document.getElementsByClassName('favorite-btn');
+    toggleFavorites = document.getElementsByClassName('favorite-btn');
 
-    // add an index to each button (for later usage to show recipe)
+    // Add an index to each button (for later usage to show recipe)
     for (let i = 0; i < buttonsRecipe.length; i++) {
         buttonsRecipe[i].dataset.index = i.toString();
     }
 
-}
-
-// clickhandler (to show recipe)
-function showRecipe(e) { 
-    
-    // make sure it is a button and belong to right class
-    if (e.target.nodeName !== "BUTTON" || e.target.classList.contains("recipe-btn") === false) {
-
-        return;
-        
+    // Add an index to each button (for later usage to add and delete
+    // favorites)
+    for (let i = 0; i < buttonsRecipe.length; i++) {
+        toggleFavorites[i].dataset.index = i.toString();
     }
 
-    // get button and id of button for the right info
+}
+
+// Function to check what function has to be executed
+function clickHandler(e) {
+
+    if (e.target.nodeName !== "BUTTON") {
+        
+        return
+
+    } else if (e.target.classList.contains("recipe-btn") === true) {
+
+        showRecipe(e);
+
+    } else if (e.target.classList.contains("favorite-btn") === true) {
+
+        addFavorite(e);
+
+    } else {
+        
+        /*  
+            In case there is more buttons, which do not have either of these classes.
+            You could argue a bit unnessecary, but in case we would want to add more
+            buttons, you wouldn't have to worry about it executing a wrong function.
+        */
+        return;
+
+    }
+
+}
+
+function showRecipe(e) { 
+    console.log("recipe")
+
+    // Get button and id of button for the right info
     let btn = e.target;
     let btnIndex = btn.dataset.index;
+    console.log(btnIndex)
     
-    let listOfRecipes = [
-        {   
-            description: 'Put it in the oven and go!',
-            tags: 'cheese, oven'
-        },
-        {
-            description: 'Cheesy Macaroni, straight out of the oven!',
-            tags: 'cheese, oven, mac'
-        },
-        {
-            description: 'Available for self-cooking or as an instant.',
-            tags: 'omnomnom, noodles, instant'
-        },
-        {
-            description: 'Everytime in the city after midnight',
-            tags: 'kapsalon, tasty, meat'
-        },
-        {
-            description: 'Specialty when on holiday in Spain',
-            tags: 'fish, rice'
-        },
-        {   
-            description: 'Order or make it yourself!',
-            tags: 'fish, seaweed, food'
-        },
-        {
-            description: 'Meat (pork or chicken) served with potato, onion, tomato, tzatziki or yogurt!',
-            tags: 'meat, fresh vegetables, pork, chicken'
-        },
-        {
-            description: 'Straight out of heaven.',
-            tags: 'omnomnom, cheese, oven'
-        },
-        {
-            description: 'Famous in Berlin, lovely to eat.',
-            tags: 'bread, potato, tasty, meat'
-        },
-        {
-            description: 'Цыка блять, советы идут',
-            tags: 'pancake, savory, sweet'
-        }
-    ]
-
     let recipeText = document.getElementById('recipe');
     let tagsText = document.getElementById('tags');
     recipeText.innerHTML = listOfRecipes[btnIndex].description;
     tagsText.innerHTML = listOfRecipes[btnIndex].tags;
 
 }
+
+function addFavorite(e) {
+    
+}
+
