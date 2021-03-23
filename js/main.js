99160f202d2498d5f35cd59285be19b1e917aa5c
TzunderWulf <svandenbos@tzunder.nl>
2021-03-23 18:03:56 +0100

    All functions now working and fully dynamic

--- js/main.js
+++ js/main.js
@@ -1,149 +1,182 @@
+// Add eventlistener to start functions, after window is loaded.
 window.addEventListener('load', init);
 
-let btn;
-let btnIndex;
-let recipeDiv;
-let storedString;
-let storedData
-let listOfRecipes = [
-    {   
-        description: 'Put it in the oven and go!',
-        tags: 'cheese, oven'
-    },
-    {
-        description: 'Cheesy Macaroni, straight out of the oven!',
-        tags: 'cheese, oven, mac'
-    },
-    {
-        description: 'Available for self-cooking or as an instant.',
-        tags: 'omnomnom, noodles, instant'
-    },
-    {
-        description: 'Everytime in the city after midnight',
-        tags: 'kapsalon, tasty, meat'
-    },
-    {
-        description: 'Specialty when on holiday in Spain',
-        tags: 'fish, rice'
-    },
-    {   
-        description: 'Order or make it yourself!',
-        tags: 'fish, seaweed, food'
-    },
-    {
-        description: 'Meat (pork or chicken) served with potato, onion, tomato, tzatziki or yogurt!',
-        tags: 'meat, fresh vegetables, pork, chicken'
-    },
-    {
-        description: 'Straight out of heaven.',
-        tags: 'omnomnom, cheese, oven'
-    },
-    {
-        description: 'Famous in Berlin, lovely to eat.',
-        tags: 'bread, potato, tasty, meat'
-    },
-    {
-        description: 'Цыка блять, советы идут',
-        tags: 'pancake, savory, sweet'
-    }
-]
+const recipes = document.getElementById('recipes'); // Section housing the dishes
+let webserviceURL; let btn; let btnId; let btnParent; let storedString; let storedData;
 let favorites = [];
 
 function init() {
-    let recipesSection = document.getElementById('recipes');
-    recipesSection.addEventListener('click', clickHandler);
+    getData();
 
-    let buttonsRecipes = document.getElementsByClassName('recipe-btn');
-    let toggleFavorites = document.getElementsByClassName('favorite-btn');
-
-    // We can use the same for loop for both elements, every recipe
-    // will always have to have two buttons.
-    for (let i = 0; i < buttonsRecipes.length; i++) {
-        buttonsRecipes[i].dataset.index = i.toString();
-        toggleFavorites[i].dataset.index = i.toString();
-    }
-
+    recipes.addEventListener('click', clickHandler);
+    
+    /*
+        Get old favorites and push them back into favorites array, so if you add
+        new favorites, you also get the old ones back when reloading.
+    */
     storedString = localStorage.getItem('favorites');
 
     if (storedString != undefined) {
         storedData = JSON.parse(storedString);
         for (let favorite of storedData) {
-            fillFromStorage(favorite);
+            favorites.push(favorite)
         }
     }
 }
 
-// clickHandler, to make sure what function has to be executed
+/* 
+    Function clickHandler, to check what needs to be executed, when
+    one of the two buttons is pressed.
+*/
 function clickHandler(e) {
     if (e.target.nodeName !== 'BUTTON') {
         return;
-    } else if (e.target.classList.contains('recipe-btn')) {
-        showRecipe(e);
     } else if (e.target.classList.contains('favorite-btn')) {
         if (e.target.parentNode.classList.contains('favorite-recipe')) {
-            deleteFavorite(e);
+            console.log('remove')
+            removeFromFavorites(e);
         } else {
-            addFavorite(e);
+            addToFavorite(e);
         }
-    } else {
-        // In case there is more buttons, which do not have either classes.
-        return
+    } else if (e.target.classList.contains('recipe-btn')) {
+        showRecipe(e);
     }
 }
 
-// showRecipe, shows the recipe and tags in detailview
+/*
+    Function showRecipe, when the recipe button is clicked, show the
+    recipe in the detail view (aside element)
+*/
 function showRecipe(e) {
-    // Get the button and id, for the right information.
     btn = e.target;
-    btnIndex = btn.dataset.index;
+    btnId = btn.dataset.id;
+    webserviceURL = `webservice/index.php?id=${btnId}`;
 
-    let recipe = document.getElementById('recipe');
-    let tags = document.getElementById('tags');
-    recipe.innerHTML = listOfRecipes[btnIndex].description;
-    tags.innerHTML = listOfRecipes[btnIndex].tags;
+    fetch(webserviceURL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let recipe = document.getElementById('show-recipe');
+            recipe.innerText = data.recipe;
+
+            let tags = document.getElementById('show-tags');
+            tags.innerText = data.tags;
+        });
 }
 
-// addFavorite, add class and to localstorage
-function addFavorite(e) {
+/*
+    Function addToFavorite, when the favorite button is clicked, we
+    then add that to localstorage and give it a special class.
+    Once clicked, changes text within the button to "remove from 
+    favorites".
+*/
+function addToFavorite(e) {
     btn = e.target;
-    btnIndex = btn.dataset.index;
-    recipeDiv = btn.parentNode;
-    recipeDiv.classList.add('favorite-recipe');
+    btnId = btn.dataset.id;
+    btn.innerText = 'Remove from favorites';
+    btnParent = btn.parentNode;
+    btnParent.classList.add('favorite-recipe');
 
-    favorites.push(btnIndex);
+    favorites.push(btnId);
     localStorage.setItem('favorites', JSON.stringify(favorites));
 }
 
-// deleteFavorite, remove class and from localstorage
-function deleteFavorite(e) {
+/*
+    Function removeFromFavorites, when the favorite button is clicked 
+    again, we then remove it from localstorage and the class. We also
+    reset the text within the button to "Add to favorites". 
+*/
+function removeFromFavorites(e) {
     btn = e.target;
-    btnIndex = btn.dataset.index;
-    recipeDiv = btn.parentNode;
-    recipeDiv.classList.remove('favorite-recipe');
+    btnId = btn.dataset.id;
+    btn.innerText = 'Add to favorites';
+    btnParent = btn.parentNode;
+    btnParent.classList.remove('favorite-recipe');
 
+    // Get the data and parse it
     storedString = localStorage.getItem('favorites');
     storedData = JSON.parse(storedString);
 
-    // Get index of the index of the item we want to remove
-    // within the array.
-    let index = storedData.indexOf(btnIndex);
+    // Get the index of the item in the array
+    let storedArrayIndex = storedData.indexOf(btnId);
+    let favoritesArrayIndex = favorites.indexOf(btnId);
 
-    // Remove the index.
-    storedData.splice(index, index + 1);
+    // Remove from the array (storedData) and the favorites array
+    storedData.splice(storedArrayIndex, storedArrayIndex + 1);
+    favorites.splice(favoritesArrayIndex, favoritesArrayIndex + 1);
 
-    // Update the storage.
+    // Update the storage (reset it)
     localStorage.setItem('favorites', JSON.stringify(storedData));
 }
 
-// fillFromStorage, fill in from the localstorage
-function fillFromStorage(index) {
-    let recipeDivs = document.getElementsByClassName('recipe');
-
-    // For every div check if the index is found in the localstorage.
-    for (let i = 0; i < recipeDivs.length; i++) {
-        recipeDivs[i].dataset.index = i.toString();
-        if (recipeDivs[i].dataset.index == index) {
-            recipeDivs[i].classList.add('favorite-recipe');
+/*
+    Function fillInFromLocalstorage, once the page is reloaded, we want
+    our favorites to be returned and given the class.
+*/
+function fillInFromLocalstorage(div) {
+    storedString = localStorage.getItem('favorites');
+    if (storedString != undefined) {
+        storedData = JSON.parse(storedString);
+        for (let favorite of storedData) {
+            if (div.dataset.id === favorite) {
+                div.classList.add('favorite-recipe');
+            }
         }
     }
+}
+
+/*
+    Function getData, once the page loads, get the data and print it to
+    the page (creating tags) if succesfull. If not give a error.
+*/
+function getData() {
+    webserviceURL = 'webservice/index.php';
+    fetch(webserviceURL)
+        .then((response) => response.json())
+        .then(getDataSuccess)
+        .catch(getDataFail)
+}
+
+// Function getDataSuccess, if its a success we then create the elements.
+function getDataSuccess(data) {
+    for (let item of data) {
+        // Create all elements to put in information
+        const recipeDiv = document.createElement('div');
+        recipeDiv.classList.add('recipe');
+        recipeDiv.dataset.id = item.id;
+        recipes.appendChild(recipeDiv);
+
+        const title = document.createElement('h2');
+        title.classList.add('title-recipe');
+        title.innerText = item.name; // Return the name of the object
+        recipeDiv.appendChild(title);
+
+        const img = document.createElement('img');
+        img.classList.add('image-recipe');
+        img.setAttribute('alt', item.name)
+        recipeDiv.appendChild(img);
+
+        fillInFromLocalstorage(recipeDiv);
+
+        const favoriteBtn = document.createElement('button');
+        favoriteBtn.classList.add('standard-btn', 'favorite-btn');
+        if (recipeDiv.classList.contains('favorite-recipe')) {
+            favoriteBtn.innerText = 'Remove from favorites';
+        } else {
+            favoriteBtn.innerText = 'Add to favorites';
+        }
+        favoriteBtn.dataset.id = item.id;
+        recipeDiv.appendChild(favoriteBtn);
+
+        const recipeBtn = document.createElement('button');
+        recipeBtn.classList.add('standard-btn', 'recipe-btn');
+        recipeBtn.innerText = 'Show recipe';
+        recipeBtn.dataset.id = item.id;
+        recipeDiv.appendChild(recipeBtn);
+    }
+}
+
+// Function getDataFail, if it doesn't get data, give an error message back.
+function getDataFail(data) {
+
 }
